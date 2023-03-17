b4df45323709f773afcef920a819261e84095304
HEXZ_ <lathesh780@gmail.com>
2023-03-17 12:48:25 +0530

    Update XGame.cpp

--- XGame.cpp
+++ XGame.cpp
@@ -1,5 +1,16 @@
 #include "raylib.h"
 int main()
 {
-    InitWindow(100,100,"Test");
+    int width = 1080;
+    int height = 920;
+    InitWindow(width,height,"Test");
+
+    while (!WindowShouldClose())
+    {
+   
+    BeginDrawing();
+    ClearBackground(RED);
+    DrawCircle(500,500,20,BLUE);
+    EndDrawing();
+    }
 }
